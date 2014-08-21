7433d0e6eb5ad70e2bb44a6de30ba3493317cc8f
kanehara <kanehara@umich.edu>
2014-08-20 23:50:07 -0400

    First commit

---
+++ Streamline/CustomViewController.m
@@ -0,0 +1,29 @@
+//
+//  CustomViewController.m
+//  Streamline
+//
+//  Created by kanehara on 8/20/26 H.
+//  Copyright (c) 26 Heisei kanehara. All rights reserved.
+//
+
+#import "CustomViewController.h"
+#import "SWRevealViewController.h"
+
+@interface CustomViewController ()
+
+@end
+
+@implementation CustomViewController
+
+- (void) viewDidLoad {
+    // allocate sideBarButton
+    _sideBarButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menu"] style: UIBarButtonItemStylePlain target:nil action:nil];
+    
+    // sideBarButton config
+    _sideBarButton.tintColor = [UIColor colorWithWhite:.3 alpha:1];
+    _sideBarButton.target = self.revealViewController;
+    _sideBarButton.action = @selector(revealToggle:);
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+}
+
+@end
---
+++ Streamline/ShopViewController.m
@@ -0,0 +1,22 @@
+//
+//  ShopViewController.m
+//  Streamline
+//
+//  Created by kanehara on 8/20/26 H.
+//  Copyright (c) 26 Heisei kanehara. All rights reserved.
+//
+
+#import "ShopViewController.h"
+
+@interface ShopViewController ()
+
+@end
+
+@implementation ShopViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
